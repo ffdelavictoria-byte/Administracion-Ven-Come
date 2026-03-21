789475cee080efec1bb770c4a408407035deda33
ffdelavictoria-byte <ffdelavictoria@gmail.com>
2026-03-20 18:07:20 -0600

    Update models.py

--- Momias/Sistema_Momias/models.py
+++ Momias/Sistema_Momias/models.py
@@ -1,14 +1,5 @@
 from django.db import models
 
-class ConfigSalario(models.Model):
-    puesto = models.CharField(max_length=150, unique=True)
-    monto = models.DecimalField(max_digits=10, decimal_places=2)
-    ultima_modificacion = models.DateTimeField(auto_now=True)
-
-    def __str__(self):
-        return f"{self.puesto} - ${self.monto}"
-
-
 class Empleado(models.Model):
     codigo_empleado = models.CharField(max_length=20, unique=True)
     nombre = models.CharField(max_length=100)
